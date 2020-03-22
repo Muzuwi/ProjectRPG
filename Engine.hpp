e7cd12eccfbb0f52d4366d175076dea57611a2f2
Muzuwi <kedziora99@gmail.com>
2020-03-22 22:14:51 +0100

    Przywrócenie nagłówków

---
+++ Engine.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <memory>
+#include <SFML/Graphics.hpp>
+#include "TextureManager.hpp"
+#include "Map.hpp"
+#include "Entity/Player.hpp"
+
+class Engine
+{
+private:
+	const unsigned windowWidth = 800;
+	const unsigned windowHeight = 600;
+
+	std::shared_ptr<sf::RenderWindow> window;
+
+	TextureManager textureManager;
+//	OverworldManager overworld;
+
+	Map newMap;
+
+	Player tempPlayer;
+
+	void RenderTile(sf::RenderTarget&);
+	void RenderTilePass2(sf::RenderTarget&);
+	void RenderEntity(sf::RenderTarget&);
+
+	bool Init();
+	void MainLoop();
+	void LoadTextures();
+	void RenderFrame();
+	void ProcessInput();
+	void Update();
+	void LoadMap();
+
+public:
+	Engine();
+	~Engine();
+
+	void Start();
+};
+
---
+++ Entity/Player.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "SFML/Window/Keyboard.hpp"
+#include "Entity/Actor.hpp"
+#include "RenderableObject.hpp"
+
+class Player final : protected Actor, protected RenderableObject {
+	unsigned frameCounter;
+	bool isMoving;
+public:
+	Player()
+			: Actor(0, 4), frameCounter(0), isMoving(false) { }
+
+	virtual void draw(Vec2u, sf::RenderTarget&) override {};
+	virtual void draw(sf::RenderTarget& target) override;
+	virtual Vec2u getDimensions() const override;
+	virtual void frameTick() override {};
+
+	Vec2f getWorldPosition() const { return worldPosition; }
+	Vec2f getSpritePosition() const { return spritePosition; }
+
+	void move(Direction dir) override;
+	void go_to(Vec2f f) override;
+	void update() override;
+protected:
+	void onInteract() override;
+	void onStep() override;
+	bool collisionCheck(Actor &entity) override;
+};
